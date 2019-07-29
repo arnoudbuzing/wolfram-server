40fbde7bb457bc2e27f234dd452f4a54a884d641
Arnoud Buzing <arnoudb@wolfram.com>
2019-07-29 13:59:49 -0500

    updates

--- clients/ruby/ruby-client.rb
+++ clients/ruby/ruby-client.rb
@@ -1,7 +1,11 @@
+# not yet working, somehow the body data 'hi' arrives before the http headers (?)
+
 require 'net/http'
 
 http = Net::HTTP.new('127.0.0.1', '5858')
+http.set_debug_output($stdout)
 request = Net::HTTP::Post.new('/')
+request['Content-Type'] = 'text/plain'
 request.body = 'hi'
 response = http.request(request)
 print response.body
