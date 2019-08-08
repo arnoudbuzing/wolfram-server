abe157103c47d20cdd8c5f9690110c3463ea7f56
Arnoud Buzing <arnoudb@wolfram.com>
2019-08-08 12:51:30 -0500

    add codemirror example

--- clients/R/r-client.r
+++ clients/R/r-client.r
@@ -4,6 +4,6 @@ r <- POST('http://127.0.0.1:5858', body = 'DateString[]' )
 
 print(content(r))
 
-r <- POST('http://127.0.0.1:5858', body = 'RandomVariate[ NormalDistribution[], 10]' )
+r <- POST('http://127.0.0.1:5858', body = 'RandomVariate[ NormalDistribution[], 3]' )
 
 print(content(r))
