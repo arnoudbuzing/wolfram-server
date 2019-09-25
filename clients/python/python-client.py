20f34d6dc818ce5250a19bb8aa4da6be68e0b82d
Arnoud Buzing <arnoudb@wolfram.com>
2019-09-25 13:45:08 -0500

    updates

--- clients/python/python-client.py
+++ clients/python/python-client.py
@@ -3,10 +3,6 @@ import requests
 
 url = 'http://127.0.0.1:5858'
 
-post = '''
-WolframAlpha["population of the netherlands", "Result"]
-'''
-
 r = requests.post(url=url, data=sys.argv[1])
 
 print('result: ' + r.text)
